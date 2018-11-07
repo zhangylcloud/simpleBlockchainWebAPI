cf13e44515d296f6e6b030bda402d75396bbb216
Cloud Zhang <zhangylcloud@outlook.com>
2018-11-07 02:42:47 -0800

    update readme and test cases

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # simpleBlockchainWebAPI
-For Udacity project 3
+## For Udacity project 3
+
+## Node Framework:
+Express
+
+## Endpoint
+1) GET:
+  http://localhost:8000/block/#
+  where # is the block height you want to get
+
+2) POST:
+  http://localhost:8000/block
+
+## How to install
+1) Clone the project from https://github.com/zhangylcloud/simpleBlockchainWebAPI.git
+2) In project root directory run `npm install`
+3) run `node index.js` 
+
+## How to test
+In tests directory, run ./testBlockchain.sh
+It contains very simple cURL GET and POST requests:
+1) Get the genesis block, should see genesis block returned
+2) Post a new block with payload "a new block", we should see the new added block printed
+3) Get a block that does not exist (height -1), we should see an error returned
+4) Post a block with empty payload, we should see an error returned 
+
+
+
--- tests/testBlockchain.sh
+++ tests/testBlockchain.sh
@@ -2,11 +2,16 @@
 echo "/////Please only run this test script after the service is running"
 echo "/////This should return the genesis block"
 curl -H "Content-Type: application/json" -X GET http://localhost:8000/block/0
+printf "\n"
 
-echo
 echo "/////This should add a new block to the chain, and we should see the newly added block"
 curl -d '{"body":"a new block"}' -H "Content-Type: application/json" -X POST http://localhost:8000/block
+printf "\n"
 
-echo
-echo "/////This should return the block at height 1"
-curl -H "Content-Type: application/json" -X GET http://localhost:8000/block/1
+echo "/////This should return a error because we are trying to get a non-existing block"
+curl -H "Content-Type: application/json" -X GET http://localhost:8000/block/-1
+printf "\n"
+
+echo "/////This should return a error because we are trying to post a block with empty payload"
+curl -d '{"body":""}' -H "Content-Type: application/json" -X POST http://localhost:8000/block
+printf "\n"
